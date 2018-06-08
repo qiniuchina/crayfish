e796653674ff44de905f5e12cd34e3e502fd4bba
qiniuno1 <39290155+qiniuno1@users.noreply.github.com>
2018-06-08 11:07:46 +0800

    Add files via upload

---
+++ com/dxc/stock/stock_morning_star_result.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import pandas as pd
+import numpy as np
+import tushare as ts
+import os
+import time
+import math
+from concurrent import futures
+from sqlalchemy import create_engine
+
+#创建引擎
+DAYS = 7
+EARN = 10
+engine = create_engine("mysql+pymysql://root:123456@16.158.102.166:3306/darklight?charset=utf8", max_overflow=5)
+def worker(para):
+    df_all =ts.get_k_data(code=para[1])
+    df_filter = df_all[df_all["date"]>=para[2]]
+    rows = df_filter.iloc[:,0].size
+    if(rows < DAYS and rows > 0):
+        first_close = df_filter.iat[0,2]
+        last_close = df_filter.iat[rows-1,2]
+        rate = round(((last_close - first_close) / first_close) * 100, 2)
+        if(rate.item() >= EARN):
+            engine.execute("UPDATE kpi_morning_star SET stage_end_datestr=%s,day_cut_off_yield=%s where id=%s",(time.strftime('%Y-%m-%d'),rate.item(), para[0]))
+    elif(rows >= DAYS):
+        first_close = df_filter.iat[0,2]
+        last_close = df_filter.iat[DAYS-1,2]
+        rate = round(((last_close - first_close) / first_close) * 100, 2)
+        engine.execute("UPDATE kpi_morning_star SET stage_end_datestr=%s,day_cut_off_yield=%s where id=%s",(time.strftime('%Y-%m-%d'),rate.item(), para[0]))
+cur = engine.execute("select * from kpi_morning_star where stage_end_datestr is null")
+stocks = cur.fetchall()
+if(len(stocks)>0):
+    executor = futures.ThreadPoolExecutor(max_workers=10)    
+    for stock in stocks : 
+        task = executor.submit(worker, stock)
+        task.done()
